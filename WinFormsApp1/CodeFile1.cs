0c11ea09ea2a7f5f6eee796ac6098ceeb924a8cb
Bert-10 <misha.rogrant@mail.ru>
2021-11-02 18:59:00 +0800

    Добавьте файлы проекта.

---
+++ WinFormsApp1/CodeFile1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// БУДТЕ ВНИМАТЕЛЬНЕЕ: ТУТ ДОЛЖЕН СТОЯТЬ ТОТ ЖЕ namespace что и в Program.cs
+namespace WindowsFormsApp1
+{
+    public class Drinkables
+    {
+    }
+    //сок
+    public enum JuicesType { apple, orange };
+    public class Juice : Drinkables
+    {
+        public JuicesType type = JuicesType.apple; 
+        public bool Pulp = false; 
+    }
+    //газировка
+    public enum GasType { cola, sprite };
+    public class Gas : Drinkables
+    {
+        public int Count = 0;
+        public GasType type = GasType.cola; // тип
+    }
+    //алкоголь
+    public enum AlcoType { vodka, vine };
+    public class Alco : Drinkables
+    {
+        public AlcoType type = AlcoType.vine;
+        public int BonesNumber = 0; 
+
+    }
+
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+//using CodeFile1;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        List<Drinkables> DrinkablesList = new List<Drinkables>();
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInfo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DrinkablesList.Clear();
+            for (var i = 0; i < 10; ++i)
+            {
+                this.DrinkablesList.Add(new Juice());
+            }
+            ShowInfo();
+        }
+
+        private void ShowInfo()
+        {
+            int JuiceCount = 0;
+            int GasCount = 0;
+            int AlcoCount = 0;
+
+            foreach (var fruit in this.DrinkablesList)
+            {
+
+                if (fruit is Juice) 
+                {
+                    JuiceCount += 1;
+                }
+                else if (fruit is Gas)
+                {
+                    GasCount += 1;
+                }
+                else if (fruit is Alco)
+                {
+                    AlcoCount += 1;
+                }
+            }
+
+            richTextBox1.Text = "Сок\tГазировка\tАлкоголь";
+            richTextBox1.Text += "\n";
+            richTextBox1.Text += String.Format("{0}\t{1}\t\t{2}", JuiceCount, GasCount, AlcoCount);
+        }
+    }
+}
